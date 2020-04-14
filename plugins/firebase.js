1bc689a151eecea8bfbeefc9ce14308670fb188f
Mike <mikestopford32@gmail.com>
2020-04-14 23:30:15 +0100

    Updated to remove credentials

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 let firebaseConfig = {
-  apiKey: "AIzaSyCgoG0YbYNo1cxvo-QP3TYmRy-w63vHKBg",
-  authDomain: "nuxt-auth-tutorial.firebaseapp.com",
-  databaseURL: "https://nuxt-auth-tutorial.firebaseio.com",
-  projectId: "nuxt-auth-tutorial",
-  storageBucket: "nuxt-auth-tutorial.appspot.com",
-  messagingSenderId: "356073305559",
-  appId: "1:356073305559:web:60c211bdb96b9178180792"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : '';
